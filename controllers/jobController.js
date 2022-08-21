1488d404c7abd5249ab13b5ce7d667c71ce05bfb
BhanukaAGH <it20009472@my.sliit.lk>
2022-08-21 19:57:25 +0530

    job routes created

--- controllers/jobController.js
+++ controllers/jobController.js
@@ -1,21 +1,56 @@
+const Job = require('../models/Job')
+const { StatusCodes } = require('http-status-codes')
+const CustomError = require('../errors')
+
+//! POST JOB
 const createJob = async (req, res) => {
-  res.send('post Job')
+  req.body.company = req.user.userId
+  const job = await Job.create(req.body)
+  res.status(StatusCodes.CREATED).json(job)
 }
 
+//! GET JOB
 const getJob = async (req, res) => {
-  res.send('Get Job Details')
+  const { id: jobId } = req.params
+  const job = await Job.findOne({ _id: jobId })
+
+  if (!job) {
+    throw new CustomError.NotFoundError(`No job with id : ${jobId}`)
+  }
+  res.status(StatusCodes.OK).json(job)
 }
 
+//! GET ALL JOB
 const getAllJobs = async (req, res) => {
-  res.send('Get All Jobs Details')
+  const jobs = await Job.find({})
+  res.status(StatusCodes.OK).json({ jobs, count: jobs.length })
 }
 
+//! UPDATE JOB
 const updateJob = async (req, res) => {
-  res.send('Job Update')
+  const { id: jobId } = req.params
+  const job = await Job.findOneAndUpdate({ _id: jobId }, req.body, {
+    new: true,
+    runValidators: true,
+  })
+
+  if (!job) {
+    throw new CustomError.NotFoundError(`No job with id : ${jobId}`)
+  }
+  res.status(StatusCodes.OK).json(job)
 }
 
+//! DELETE JOB
 const deleteJob = async (req, res) => {
-  res.send('Job Delete')
+  const { id: jobId } = req.params
+  const job = await Job.findOne({ _id: jobId })
+
+  if (!job) {
+    throw new CustomError.NotFoundError(`No job with id : ${jobId}`)
+  }
+
+  await job.remove()
+  res.status(StatusCodes.OK).json({ msg: 'Success! Job removed.' })
 }
 
 module.exports = {
