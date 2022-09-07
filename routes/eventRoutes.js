92da9356097e664daeaf32ed5c79bc449a336963
Buddheesha Induwara <it20031602@my.sliit.lk>
2022-09-07 08:30:27 +0530

    event routes updated

--- routes/eventRoutes.js
+++ routes/eventRoutes.js
@@ -1,4 +1,6 @@
 const express = require('express')
+const router = express.Router()
+
 const {
   createEvent,
   getEvent,
@@ -6,7 +8,6 @@ const {
   updateEvent,
   deleteEvent,
 } = require('../controllers/eventController')
-const router = express.Router()
 
 const {
   authenticateUser,
