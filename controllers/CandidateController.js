4ffb531896b4f28198714cd0a98c071eac9cc896
Rivindu2 <rivigith@gmail.com>
2022-09-09 03:41:56 +0530

    profile done

--- controllers/CandidateController.js
+++ controllers/CandidateController.js
@@ -2,6 +2,7 @@ const Job = require("../models/Job");
 const Event = require("../models/Event");
 const { StatusCodes } = require("http-status-codes");
 const User = require("../models/User");
+const Resume=require("../models/UserResume")
 const {getALLSavedEvents,saveEvent,getSavedEvents,deleteSavedEvents}=require("../controllers/CandidateSubcontrollers/EventsController")
 const {getALLSavedJobs,saveJob,getSavedJobs,deleteSavedJobs}=require("../controllers/CandidateSubcontrollers/JobsController")
 
@@ -51,7 +52,87 @@ const getAllEvents = async (req, res) => {
   }
 };
 
+const AddResume = async (req, res) => {
+  const { userID, skills,Location,PrimaryRole,Statement } = req.body;
+  try {
+    const find = await Resume.findOne({ userID });
+    if(find){
+      res.send({ msg: "Resume already Added" });
+      return;
+    }
+    const add = await Resume.create({
+      userID,
+      skills,
+      Location,
+      PrimaryRole,
+      Statement
+    })
+    if (add) {
+      res.status(StatusCodes.OK).send({ msg: "Added" });
+    } else {
+      res.status(StatusCodes.OK).send({ msg: "Error Adding" });
+      return;
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(StatusCodes.OK).send({ msg: "Error Adding" });
+    return;
+  }
+}
+const getUserResume = async (req, res) => {
+  const { userID: userID } = req.params;
 
+  try {
+    const find = await Resume.findOne({ userID });
+    res.status(StatusCodes.OK).json({
+      find,
+    });
+  } catch (error) {
+    res.status(400).send({ msg: "eror in retriving resume" });
+    throw new Error("eror in retriving resume");
+  }
+}
+const UpdateResume = async (req, res) => {
+  const { userID, skills,Location,PrimaryRole,Statement } = req.body;
+  const filter = { userID: userID }
+  try {
+    
+    const add = await Resume.updateOne(filter,{
+      userID,
+      skills,
+      Location,
+      PrimaryRole,
+      Statement
+    })
+    if (add) {
+      res.status(StatusCodes.OK).send({ msg: "updated" });
+    } else {
+      res.status(StatusCodes.OK).send({ msg: "Error updated" });
+      return;
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(StatusCodes.OK).send({ msg: "Error updated" });
+    return;
+  }
+}
+const RemoveResume = async (req, res) => {
+  const { userID: userID } = req.params;
+  try {    
+    const remove = await Resume.deleteOne({ userID: userID });
+
+    if (remove) {
+      res.status(StatusCodes.OK).send({ msg: "removed" });
+    } else {
+      res.status(StatusCodes.OK).send({ msg: "Error removing" });
+      return;
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(StatusCodes.OK).send({ msg: "Error removing" });
+    return;
+  }
+}
 
 module.exports = {
   getAllJobs,
@@ -63,7 +144,11 @@ module.exports = {
   getSavedEvents,
   deleteSavedEvents,
   getALLSavedJobs, 
-  getALLSavedEvents 
+  getALLSavedEvents,
+  AddResume,
+  getUserResume,
+  UpdateResume,
+  RemoveResume
 };
 //
 
--- models/UserResume.js
+++ models/UserResume.js
@@ -5,35 +5,23 @@ const UserResumeSchema = new mongoose.Schema(
     userID: {
       type: mongoose.Types.ObjectId,
       ref: 'User',
+      unique:true,
       required: [true, 'Please provide user id'],
     },
-    skills: [
-        {
-          skill: {
-            type: String,
-            default: 'empty',
-          },
-          YOE: {
-            type: Number,
-            default: 0,
-          },
-        },
-      ],
+    skills: {
+      type: Array,
+    },
     CV:{
         type: String,
-        required: true
     },
     Location:{
         type: String,
-        required: true
     },
     PrimaryRole:{
         type: String,
-        required: true
     },
     Statement:{
         type: String,
-        required: true
     }
   }
 )
