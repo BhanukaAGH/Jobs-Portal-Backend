24d32b632cd44508b5e0db44a6c57f3dff839f60
Buddheesha Induwara <it20031602@my.sliit.lk>
2022-09-19 08:45:15 +0530

    event category added in event model

--- models/Event.js
+++ models/Event.js
@@ -1,45 +1,47 @@
 const mongoose = require('mongoose')
 
-const eventSchema = new mongoose.Schema({
-  company: {
-    type: mongoose.Types.ObjectId,
-    ref: 'User',
-    required: [true, 'Please provide company id'],
-  },
+const eventSchema = new mongoose.Schema(
+  {
+    company: {
+      type: mongoose.Types.ObjectId,
+      ref: 'User',
+      required: [true, 'Please provide company id'],
+    },
 
-  eventTitle: {
-    type: String,
-    required: [true, 'Please provide Event title'],
-  },
-  // eventCategoery: {
-  //   type: String,
-  //   required: [true, 'Please provide event category'],
-  // },
-  // jobCategoryTargeted: {
-  //   type: String,
-  //   required: [true, 'Please provide Job Category Targeted'],
-  // },
+    eventTitle: {
+      type: String,
+      required: [true, 'Please provide Event title'],
+    },
+    eventCategory: {
+      type: String,
+      required: [true, 'Please provide event category'],
+    },
+    // jobCategoryTargeted: {
+    //   type: String,
+    //   required: [true, 'Please provide Job Category Targeted'],
+    // },
 
-  deliveryType: {
-    type: String,
-    enum: ['Virtual', 'Physical'],
-    required: [true, 'Please provide event delivery type'],
-  },
+    deliveryType: {
+      type: String,
+      enum: ['Virtual', 'Physical'],
+      required: [true, 'Please provide event delivery type'],
+    },
 
-  location: {
-    type: String,
-    required: [true, 'Please provide event location'],
-  },
-  date: {
-    type: Date,
-    // required: [true, 'Please provide event date'],
-  },
+    location: {
+      type: String,
+      required: [true, 'Please provide event location'],
+    },
+    date: {
+      type: Date,
+      // required: [true, 'Please provide event date'],
+    },
 
-  description: {
-    type: String,
-    required: [true, 'Please provide event description'],
+    description: {
+      type: String,
+      required: [true, 'Please provide event description'],
+    },
   },
-  
-},{ timestamps: true })
+  { timestamps: true }
+)
 
 module.exports = mongoose.model('Event', eventSchema)
